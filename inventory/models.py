2579d3ad775e9ffddbbbaf46b7b1a32c59c92c4c
paodanchacon <63526399+paodanchacon@users.noreply.github.com>
2023-04-09 19:33:16 -0400

    create item model (#4)
    * create item model
    
    * fix format

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    model = models.CharField(max_length=100)
+    brand = models.CharField(max_length=100)
+    quantity = models.IntegerField(default=0)
+    description = models.CharField(max_length=250)
+    # location_id = models.ForeignKey(Location, on_delete=models.CASCADE)
+    status = models.CharField(max_length=100)
+    active = models.BooleanField()
+    type = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
+    order_date = models.DateTimeField("date ordered")
+    check_date = models.DateTimeField("date published")
+
+    def __str__(self):
+        return self.model
