8cf81f2ff7d0630973edaebebd3b69cd212b53b3
Daniela Chambilla <63526399+paodanchacon@users.noreply.github.com>
2023-04-09 21:59:54 -0400

    create item_event model

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from inventory.models import Item
+from inventory.models import Item, Item_event
 
 admin.site.register(Item)
+admin.site.register(Item_event)
--- inventory/models.py
+++ inventory/models.py
@@ -6,13 +6,24 @@ class Item(models.Model):
     brand = models.CharField(max_length=100)
     quantity = models.IntegerField(default=0)
     description = models.CharField(max_length=250)
-    # location_id = models.ForeignKey(Location, on_delete=models.CASCADE)
+    #location_id = models.ForeignKey("Location", on_delete=models.CASCADE)
     status = models.CharField(max_length=100)
     active = models.BooleanField()
     type = models.CharField(max_length=100)
     category = models.CharField(max_length=100)
-    order_date = models.DateTimeField("date ordered")
-    check_date = models.DateTimeField("date published")
+    order_date = models.DateField("date ordered")
+    check_date = models.DateField("date published")
 
     def __str__(self):
         return self.model
+
+class Item_event(models.Model):
+    item_id = models.ForeignKey(Item, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField("date and time event")
+    type = models.CharField(max_length=100)
+    #location_id = models.ForeignKey("Location", on_delete=models.CASCADE)
+    status = models.CharField(max_length=100)
+    #customer_id = models.ForeignKey("Customer", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.type
