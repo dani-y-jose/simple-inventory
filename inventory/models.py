83ed88539ed6524a85c6b5c9d931a964a372fd0c
Daniela Chambilla <63526399+paodanchacon@users.noreply.github.com>
2023-04-11 22:03:16 -0400

    create customer model

--- inventory/models.py
+++ inventory/models.py
@@ -18,13 +18,24 @@ class Item(models.Model):
         return self.model
 
 
+class Customer(models.Model):
+    full_name = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    type = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.full_name
+
+
 class ItemEvent(models.Model):
     item_id = models.ForeignKey(Item, on_delete=models.CASCADE)
     timestamp = models.DateTimeField("date and time event")
     type = models.CharField(max_length=100)
     # location_id = models.ForeignKey("Location", on_delete=models.CASCADE)
     status = models.CharField(max_length=100)
-    # customer_id = models.ForeignKey("Customer", on_delete=models.CASCADE)
+    customer_id = models.ForeignKey(
+        Customer, null=True, blank=True, on_delete=models.PROTECT
+    )
 
     def __str__(self):
         return self.type
