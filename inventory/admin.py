847c0118dcee579721154192eeaf5e3690c9e79d
Daniela Chambilla <63526399+paodanchacon@users.noreply.github.com>
2023-04-12 23:55:25 -0400

    create location model

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from inventory.models import Item, ItemEvent, Customer
+from inventory.models import Item, ItemEvent, Customer, Location
 
 admin.site.register(Item)
 admin.site.register(ItemEvent)
 admin.site.register(Customer)
+admin.site.register(Location)
