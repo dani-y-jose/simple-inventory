e322ba751bf54ed448c3bc36b6612446c7b7b0b0
paodanchacon <63526399+paodanchacon@users.noreply.github.com>
2023-04-12 23:16:03 -0400

    create customer model (#7)

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from inventory.models import Item, ItemEvent
+from inventory.models import Item, ItemEvent, Customer
 
 admin.site.register(Item)
 admin.site.register(ItemEvent)
+admin.site.register(Customer)
