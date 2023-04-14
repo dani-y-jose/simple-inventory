3b0a9770970339858173a0e39645410a808db2f3
paodanchacon <63526399+paodanchacon@users.noreply.github.com>
2023-04-13 21:09:44 -0400

    create region model (#9)

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from inventory.models import Item, ItemEvent, Customer, Location
+from inventory.models import Item, ItemEvent, Customer, Location, Region
 
 admin.site.register(Item)
 admin.site.register(ItemEvent)
 admin.site.register(Customer)
 admin.site.register(Location)
+admin.site.register(Region)
