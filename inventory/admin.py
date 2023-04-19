dfa1f1a9c420e0c07f919ad372525889ea4afd11
paodanchacon <63526399+paodanchacon@users.noreply.github.com>
2023-04-18 21:14:24 -0400

    add status field item model as choice creating another class (#13)
    * add status field item model as choice creating another class
    
    * change the plural assignment in the admin page
    
    * create description field for status model
    
    * add description field for status model

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from inventory.models import Item, ItemEvent, Customer, Location, Region
+from inventory.models import Item, ItemEvent, Customer, Location, Region, ItemStatus
 
 admin.site.register(Item)
 admin.site.register(ItemEvent)
 admin.site.register(Customer)
 admin.site.register(Location)
 admin.site.register(Region)
+admin.site.register(ItemStatus)
--- inventory/models.py
+++ inventory/models.py
@@ -32,13 +32,27 @@ class Location(models.Model):
         return self.name
 
 
+# Example of status: Archived, Reserved, Lent, Borrowed, others
+class ItemStatus(models.Model):
+    item_status = models.CharField(max_length=100, blank=True)
+    description = models.CharField(max_length=250, null=True)
+
+    class Meta:
+        verbose_name_plural = "Item status"
+
+    def __str__(self):
+        return self.item_status
+
+
 class Item(models.Model):
     model = models.CharField(max_length=100)
     brand = models.CharField(max_length=100)
     quantity = models.IntegerField(default=0)
     description = models.CharField(max_length=250)
     location_id = models.ForeignKey(Location, null=True, on_delete=models.CASCADE)
-    status = models.CharField(max_length=100)
+    item_status = models.ForeignKey(
+        ItemStatus, null=True, blank=True, on_delete=models.PROTECT
+    )
     active = models.BooleanField()
     type = models.CharField(max_length=100)
     category = models.CharField(max_length=100)
