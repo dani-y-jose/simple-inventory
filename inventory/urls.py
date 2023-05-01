13824b38683fae0ed3ae8fa9e43cceca6963842b
Daniela Chambilla <63526399+paodanchacon@users.noreply.github.com>
2023-05-01 08:43:33 -0400

    create base-home template

--- inventory/urls.py
+++ inventory/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from inventory import views
 
 urlpatterns = [
-    path("", views.hello, name="index"),
+    # path("", views.hello, name="index"),
+    path("", views.index, name="index"),
 ]
