b0e5eec745bd017e332aa22c6b85bc1e88b58bbf
paodanchacon <63526399+paodanchacon@users.noreply.github.com>
2023-05-29 22:15:39 -0400

    create list-items in templates and views (#19)

--- inventory/urls.py
+++ inventory/urls.py
@@ -5,4 +5,6 @@ from inventory import views
 urlpatterns = [
     # path("", views.hello, name="index"),
     path("", views.index, name="index"),
+    path("list_items/", views.list_items, name="list_items"),
+    path("items/", views.items, name="items"),
 ]
